5e7923ab13d3a99ed69aa75ac6414b1661a5d535
v5302 <v5302@yandex.ru>
2019-10-03 19:01:00 +0400

    ввод и сортировка в route

--- 2.11/Main.cpp
+++ 2.11/Main.cpp
@@ -2,16 +2,48 @@
 #include<string>
 #include"Route.h"
 
-
 using namespace std;
+
+
 
 int main()
 {
 	setlocale(LC_ALL, "rus");
 	//srand(time(NULL));
-
-	Route f;
+	int const size = 4;
+	Route route[size];
+	Route temp;
 	
+
+	for (int i = 0; i < size; i++)//ввод данных
+	{
+		cin >> route[i];
+		
+	}
+
+	for (int i = 0; i < size-1; i++)//сортировка 
+	{
+		for (int j = 0; j < size-1 - i; j++)
+		{
+			if (route[j].GetNum() > route[j + 1].GetNum())
+			{
+				temp = route[j];
+				route[j] = route[j + 1];
+				route[j + 1] = temp;
+			}
+		}
+		
+	}
+
+	
+	for (int i = 0; i < size; i++)//вывод данных
+	{
+		cout << route[i] << endl;
+	}
+
+	
+	//поиск: вводим стринг и если есть совпадения то выводим рут
+
 
 
 	system("pause");
--- 2.11/Route.h
+++ 2.11/Route.h
@@ -10,10 +10,38 @@ private:
 	int num;
 public:
 	Route(): A(""), B(""), num(0) {}
+	Route(string a, string b, int n) { this->A = a; this->B = b; this->num = n; }
 	~Route(){}
 	
-	void show_route();//вывод инфы про маршрут
-	void set_route(string a, string b, int n);//установить всю инфу
+	
+	void add_route(string a, string b, int n) {A = a; B = b; num = n;}
+
+	//вывод типа  Route g; cout<<g;
+	friend std::ostream& operator <<(std::ostream &out, Route &route)
+	{
+		cout << "Route є" << route.GetNum() << endl;
+		cout << "A: " << route.GetA() << endl;
+		cout << "B: " << route.GetB() << endl << endl;
+		return out;
+	}
+
+	//ввод типа Route f; cin>>f;
+	friend std::istream& operator >>(std::istream &in, Route &route)
+	{
+		string a = "";
+		string b = "";
+		int n = 0;
+		cout << "Enter A: ";
+		cin >> a;
+		cout << endl << "Enter B: ";
+		cin >> b;
+		cout <<endl<< "¬ведите номер марштура: ";
+		cin >> n;
+		cout << endl << endl;
+		route.add_route(a, b, n);
+		
+		return in;
+	}
 
 	void SetA(string a) { A = a; }
 	void SetB(string b) { B = b; }
